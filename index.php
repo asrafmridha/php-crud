f645d8d750b139cfda9c365fe596bb90b912a77b
asraf-mridha <asrafmridha@gmail.com>
2022-07-27 21:43:22 +0600

    add button

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ include("class.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Db connect by constructor</title>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 </head>
 <body>
@@ -71,24 +73,38 @@ include("class.php");
          <th>Name</th>
          <th>Email</th>
          <th>Status</th>
+         <th>Action</th>
+
          </tr>
 
              <?php 
                $result= $save->showdata();
 
+               
+
                while($res= $result->fetch_assoc()){
              
-             
+                $slno= $res['id']=1;
              //fetch data from db
+            
              echo 
                 '<tr>
                 <td>'.$res['id'].' </td>
                 <td>'.$res['name'].' </td>
                 <td>'.$res['email'].' </td>
                 <td>'.$res['status'].' </td>
-              </tr>';
+                  <td>
+                  
+                  <button class="btn btn-info"> <i class="fa fa-solid fa-pen-to-square"></i> </button>
 
+                  <button class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+                  
+                  
+                  </td>
+
+              </tr>';
                }
+             
                
                
               
@@ -102,11 +118,7 @@ include("class.php");
       </div>
     </div>
 
-     
-
-
-   
-   
-
 </body>
 </html>
+
+
