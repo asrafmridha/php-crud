ca39dd8422c4ed13212c105716d83c045688733f
asraf-mridha <asrafmridha@gmail.com>
2022-07-27 23:57:09 +0600

    add modal //without delete without class

--- index.php
+++ index.php
@@ -101,13 +101,34 @@ include("class.php");
                   
                   <a  class="btn btn-info"> <i class="fa fa-solid fa-pen-to-square"></i> </a>
 
-                  <a href="delete.php?uid='.$res['id'].'" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
+                  <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal'.$res['id'].'" ><i class="fa-solid fa-trash"></i></button>
                   </td>
               </tr>';
               $slno++;
+  
+              ?>
+       
+              <!-- Modal -->
+   <div class="modal fade" id="exampleModal<?php echo $res['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
              
-               }
+           <?php    }
         
              ?>
 
@@ -125,8 +146,12 @@ include("class.php");
     </script>
 
      <script src="cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script> 
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 </html>
+
+
+
 
 
